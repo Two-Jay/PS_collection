a413d14315055b7029533fa1e85b37b400377d1f
Two-Jay <djeeee1272@gmail.com>
2021-09-09 04:59:09 +0900

    2908.c BOJ comp

---
+++ BOJ/2908.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fn_reverse(int *a)
+{
+	int ret = 0;
+
+	ret += (*a % 10) * 100 + (*a / 100);
+	ret += *a -= (ret % 10) * 100 + (ret / 100);
+	*a = ret;
+}
+
+int main()
+{
+	int a, b, target;
+
+	scanf("%d %d", &a, &b);
+	fn_reverse(&a);
+	fn_reverse(&b);
+	target = a > b ? a : b;
+	printf("%d\n", target);
+}
