63b20e7f03816f2b10a586554333c8e35e805dea
Two-Jay <djeeee1272@gmail.com>
2021-09-09 23:38:52 +0900

    1157.c BOJ comp

--- BOJ/1157.c
+++ BOJ/1157.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+int fn_islower_alpha(char c) { return (c >= 'a' && c <= 'z'); }
+int fn_isupper_alpha(char c) { return (c >= 'A' && c <= 'Z'); }
+
 int fn_check_memo(int *m, int len, int max)
 {
 	int ret = 0, i = 1, dupflag = 0;
@@ -20,22 +23,25 @@ int fn_check_memo(int *m, int len, int max)
 	return (dupflag ? -1 : ret);
 }
 
-int main ()
+int main (void)
 {
-	char input[1000000];
+	char input[1000001];
 	int max = 0, len, checker = -1;
 	int memo[26] = { 0, };
 
 	scanf("%s", input);
 	len = strlen(input);
-	for (int i = 0; i < len; i++)
-	{	
-		if (input[i] >= 'A' && input[i] <= 'Z')
-			memo[input[i] - 65]++;
-		else if (input[i] >= 'a' && input[i] <= 'z')
-			memo[input[i] - 97]++;
+	for (int i = 0, idx = -1; i < len; i++)
+	{
+		if (fn_isupper_alpha(input[i]))
+			idx = input[i] - 65;
+		else if (fn_islower_alpha(input[i]))
+			idx = input[i] - 97;
+		if (idx != -1)
+			memo[idx]++;
+		idx = -1;
 	}
-	for (int i = 0; i < len; i++)
+	for (int i = 0; i < 26; i++)
 		if (max < memo[i])
 			max = memo[i];
 	checker = fn_check_memo(memo, len, max);
